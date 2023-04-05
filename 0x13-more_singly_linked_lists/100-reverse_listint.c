d98c3572622dcd9d66d7e7e0a11ed6e29f894a31
root <root@DESKTOP-BIV92D1.localdomain>
2023-04-05 07:24:29 +0000

    more singly linked list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -2,11 +2,10 @@
 
 /**
  * reverse_listint - reverses a linked list
- * @head: pointer to the first node in the new list
+ * @head: pointer to the first node in the list
  *
  * Return: pointer to the first node in the new list
  */
-
 listint_t *reverse_listint(listint_t **head)
 {
 listint_t *prev = NULL;
