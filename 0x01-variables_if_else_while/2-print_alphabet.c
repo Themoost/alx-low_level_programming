42b36df102edb18d0013752e033c24913c6de150
root <root@DESKTOP-BIV92D1.localdomain>
2023-04-15 20:40:06 +0000

    if else variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the code, from a - z.
+ * Description: this program prints alphabets in lower case.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	return (0);
+}
