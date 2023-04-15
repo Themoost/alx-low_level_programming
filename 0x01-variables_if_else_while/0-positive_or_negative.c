5cdc227ea950f38392df6266dc5d3064ddfb139b
root <root@DESKTOP-BIV92D1.localdomain>
2023-04-15 13:05:17 +0000

    if else variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,15 +18,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		puts("%d is positive", n);
+		pritf("%d is positive", n);
 	}
 	else if (n < 0)
 	{
-		puts("%d is negative", n);
+		printf("%d is negative", n);
 	}
 	else
 	{
-		puts("%d is zero", n);
+		printf("%d is zero", n);
 	}
 
 	return (0);
