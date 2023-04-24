e9efe989300ceb0767a11642145ef861eab7f76a
themoost <takyikenneth97@gmail.com>
2023-04-24 22:23:29 +0000

    variables if else

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the code.
+ * Description: This program prints single digit numbers
+ * of base 10 starting from 0.
+ *
+ * Return: Always (0).
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+	printf("%d\n", num);
+	}
+	return (0);
+}
+
