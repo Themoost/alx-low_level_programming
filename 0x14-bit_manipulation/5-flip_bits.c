3b18d67678c86829a201940370bbaf7d419b5731
themoost <takyikenneth97@gmail.com>
2023-04-28 21:26:08 +0000

    bit manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -14,10 +14,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int current;
 	unsigned long int exclusive = n ^ m;
 
-	for (i = 63; i > 0; i--)
+	for (i = 63; i >= 0; i--)
 	{
 		current = exclusive >> i;
-		if (current & 1)
+		if (current & 1UL)
 			count++;
 	}
 
